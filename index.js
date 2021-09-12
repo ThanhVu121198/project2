9e898886a40998175b63ff5310f69daad7521a02
ThanhVu121198 <Thanhvu121198@gmail.com>
2021-09-12 20:04:14 +0700

    home

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
+app.listen(port,function (){
+    console.log("sever is running....");
+});
+app.use(express.static("public"));
+app.set("view engine","ejs");
+//------------ THANH--------------//
+
+app.get("/home",function (req,res) {
+    res.render("home");
+});
