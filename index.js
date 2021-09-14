962693a8becede52b2ed605e7a8ae098f39e8d14
ThanhVu121198 <Thanhvu121198@gmail.com>
2021-09-14 19:06:14 +0700

    home

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.get("/video2",function (req,res) {
     res.render("video2");
 });
 app.get("/video3",function (req,res) {
-    res.render("video2");
+    res.render("video3");
 });
 app.get("/video4",function (req,res) {
-    res.render("video2");
+    res.render("video4");
 });
 
 //--------------------HUNG-------------------//
@@ -84,6 +84,47 @@ app.get("/diaphongthuy",function (req,res) {
     res.render("diaphongthuy");
 });
 
+// -----------PHƯƠNG---------------//
 
-
-
+app.get("/news",function (req,res) {
+    res.render("news");
+});
+app.get("/Promotional",function (req,res) {
+    res.render("Promotional");
+});
+app.get("/trending",function (req,res) {
+    res.render("trending");
+});
+app.get("/tip",function (req,res) {
+    res.render("tip");
+});
+app.get("/libary",function (req,res) {
+    res.render("libary");
+});
+app.get("/new2",function (req,res) {
+    res.render("new2");
+});
+app.get("/new3",function (req,res) {
+    res.render("new3");
+});
+app.get("/new4",function (req,res) {
+    res.render("new4");
+});
+app.get("/new5",function (req,res) {
+    res.render("new5");
+});
+app.get("/new6",function (req,res) {
+    res.render("new6");
+});
+app.get("/new7",function (req,res) {
+    res.render("new7");
+});
+app.get("/new8",function (req,res) {
+    res.render("new8");
+});
+app.get("/new9",function (req,res) {
+    res.render("new9");
+});
+app.get("/new1",function (req,res) {
+    res.render("new1");
+});
